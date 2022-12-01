40b739adc9f4a8d8b5ab335197925c1ad0b76ff6
Dương Nguyễn Hải <haiduong290701@gmail.com>
2022-12-01 21:35:34 +0700

    first commit

---
+++ BTL/BaitaplonQLNS/BaitaplonQLNS/Form1.Designer.cs
@@ -0,0 +1,344 @@
+﻿namespace BaitaplonQLNS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.sidebar = new System.Windows.Forms.FlowLayoutPanel();
+            this.pnl_HomeCtn = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btt_quanly = new System.Windows.Forms.Button();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.btt_baocao = new System.Windows.Forms.Button();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.btt_dashboard = new System.Windows.Forms.Button();
+            this.HomeTimer = new System.Windows.Forms.Timer(this.components);
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.sidebar.SuspendLayout();
+            this.pnl_HomeCtn.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.panel9.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(3, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(186, 144);
+            this.panel2.TabIndex = 1;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(59, 29);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(63, 63);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // sidebar
+            // 
+            this.sidebar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.sidebar.Controls.Add(this.panel2);
+            this.sidebar.Controls.Add(this.pnl_HomeCtn);
+            this.sidebar.Controls.Add(this.panel7);
+            this.sidebar.Controls.Add(this.panel9);
+            this.sidebar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.sidebar.Location = new System.Drawing.Point(0, 0);
+            this.sidebar.MaximumSize = new System.Drawing.Size(180, 577);
+            this.sidebar.Name = "sidebar";
+            this.sidebar.Size = new System.Drawing.Size(180, 577);
+            this.sidebar.TabIndex = 3;
+            this.sidebar.Paint += new System.Windows.Forms.PaintEventHandler(this.sidebar_Paint);
+            // 
+            // pnl_HomeCtn
+            // 
+            this.pnl_HomeCtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.pnl_HomeCtn.Controls.Add(this.panel6);
+            this.pnl_HomeCtn.Controls.Add(this.panel1);
+            this.pnl_HomeCtn.Controls.Add(this.panel5);
+            this.pnl_HomeCtn.Controls.Add(this.panel3);
+            this.pnl_HomeCtn.Controls.Add(this.panel4);
+            this.pnl_HomeCtn.Location = new System.Drawing.Point(3, 153);
+            this.pnl_HomeCtn.MaximumSize = new System.Drawing.Size(186, 280);
+            this.pnl_HomeCtn.MinimumSize = new System.Drawing.Size(186, 50);
+            this.pnl_HomeCtn.Name = "pnl_HomeCtn";
+            this.pnl_HomeCtn.Size = new System.Drawing.Size(186, 50);
+            this.pnl_HomeCtn.TabIndex = 3;
+            this.pnl_HomeCtn.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint_1);
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.button5);
+            this.panel6.Location = new System.Drawing.Point(3, 230);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(186, 50);
+            this.panel6.TabIndex = 4;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.button5.Image = global::BaitaplonQLNS.Properties.Resources._298849_primitive_dot_icon;
+            this.button5.Location = new System.Drawing.Point(0, 0);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(186, 50);
+            this.button5.TabIndex = 2;
+            this.button5.Text = "Tiền lương";
+            this.button5.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.btt_quanly_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btt_quanly);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(186, 50);
+            this.panel1.TabIndex = 4;
+            // 
+            // btt_quanly
+            // 
+            this.btt_quanly.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.btt_quanly.FlatAppearance.BorderSize = 0;
+            this.btt_quanly.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btt_quanly.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btt_quanly.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btt_quanly.Image = global::BaitaplonQLNS.Properties.Resources.quanly_icon;
+            this.btt_quanly.Location = new System.Drawing.Point(0, 0);
+            this.btt_quanly.Name = "btt_quanly";
+            this.btt_quanly.Size = new System.Drawing.Size(186, 50);
+            this.btt_quanly.TabIndex = 2;
+            this.btt_quanly.Text = "Quản Lý";
+            this.btt_quanly.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btt_quanly.UseVisualStyleBackColor = false;
+            this.btt_quanly.Click += new System.EventHandler(this.btt_quanly_Click);
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.button4);
+            this.panel5.Location = new System.Drawing.Point(3, 171);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(186, 50);
+            this.panel5.TabIndex = 4;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.button4.Image = global::BaitaplonQLNS.Properties.Resources._298849_primitive_dot_icon;
+            this.button4.Location = new System.Drawing.Point(0, 0);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(186, 50);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Phòng Ban";
+            this.button4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.btt_quanly_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Location = new System.Drawing.Point(3, 59);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(186, 50);
+            this.panel3.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.button1.Image = global::BaitaplonQLNS.Properties.Resources._298849_primitive_dot_icon;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(186, 50);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Nhân viên";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.btt_quanly_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.button2);
+            this.panel4.Location = new System.Drawing.Point(3, 115);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(186, 50);
+            this.panel4.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.button2.Image = global::BaitaplonQLNS.Properties.Resources._298849_primitive_dot_icon;
+            this.button2.Location = new System.Drawing.Point(0, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(186, 50);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Chức vụ";
+            this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.btt_quanly_Click);
+            // 
+            // panel7
+            // 
+            this.panel7.Controls.Add(this.btt_baocao);
+            this.panel7.Location = new System.Drawing.Point(3, 209);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(186, 50);
+            this.panel7.TabIndex = 4;
+            // 
+            // btt_baocao
+            // 
+            this.btt_baocao.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.btt_baocao.FlatAppearance.BorderSize = 0;
+            this.btt_baocao.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btt_baocao.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btt_baocao.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btt_baocao.Image = global::BaitaplonQLNS.Properties.Resources._8956780_dashboard_bar_chart_browser_website_statistics_icon;
+            this.btt_baocao.Location = new System.Drawing.Point(0, 3);
+            this.btt_baocao.Name = "btt_baocao";
+            this.btt_baocao.Size = new System.Drawing.Size(186, 50);
+            this.btt_baocao.TabIndex = 2;
+            this.btt_baocao.Text = "Báo Cáo";
+            this.btt_baocao.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btt_baocao.UseVisualStyleBackColor = false;
+            this.btt_baocao.Click += new System.EventHandler(this.btt_baocao_Click);
+            // 
+            // panel9
+            // 
+            this.panel9.Controls.Add(this.btt_dashboard);
+            this.panel9.Location = new System.Drawing.Point(3, 265);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(186, 50);
+            this.panel9.TabIndex = 6;
+            // 
+            // btt_dashboard
+            // 
+            this.btt_dashboard.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.btt_dashboard.FlatAppearance.BorderSize = 0;
+            this.btt_dashboard.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btt_dashboard.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btt_dashboard.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btt_dashboard.Image = global::BaitaplonQLNS.Properties.Resources.hethong_icon;
+            this.btt_dashboard.Location = new System.Drawing.Point(0, 3);
+            this.btt_dashboard.Name = "btt_dashboard";
+            this.btt_dashboard.Size = new System.Drawing.Size(186, 50);
+            this.btt_dashboard.TabIndex = 2;
+            this.btt_dashboard.Text = "Hệ Thống";
+            this.btt_dashboard.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btt_dashboard.UseVisualStyleBackColor = false;
+            this.btt_dashboard.Click += new System.EventHandler(this.btt_dashboard_Click);
+            // 
+            // HomeTimer
+            // 
+            this.HomeTimer.Interval = 10;
+            this.HomeTimer.Tick += new System.EventHandler(this.HomeTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(951, 577);
+            this.Controls.Add(this.sidebar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.sidebar.ResumeLayout(false);
+            this.pnl_HomeCtn.ResumeLayout(false);
+            this.panel6.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel7.ResumeLayout(false);
+            this.panel9.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btt_baocao;
+        private System.Windows.Forms.Button btt_quanly;
+        private System.Windows.Forms.Button btt_dashboard;
+        private System.Windows.Forms.FlowLayoutPanel sidebar;
+        private System.Windows.Forms.Panel pnl_HomeCtn;
+        private System.Windows.Forms.Timer HomeTimer;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel9;
+    }
+}
+
---
+++ BTL/BaitaplonQLNS/BaitaplonQLNS/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaitaplonQLNS
+{
+    public partial class Form1 : Form
+    {
+        bool homeCollapsed;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+   
+        }
+
+        private void sidebarTimer_Tick(object sender, EventArgs e)
+        {
+ 
+        }
+
+        private void panel1_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void HomeTimer_Tick(object sender, EventArgs e)
+        {
+           if(homeCollapsed)
+            {
+                pnl_HomeCtn.Height += 10;
+                if (pnl_HomeCtn.Height == pnl_HomeCtn.MaximumSize.Height)
+                {
+                    homeCollapsed = false;
+                    HomeTimer.Stop();
+                }
+            }
+           else
+            {
+                pnl_HomeCtn.Height -= 10;
+                if(pnl_HomeCtn.Height == pnl_HomeCtn.MinimumSize.Height)
+                {
+                    homeCollapsed = true;
+                    HomeTimer.Stop();
+                }
+            }
+        }
+
+        private void btt_quanly_Click(object sender, EventArgs e)
+        {
+            HomeTimer.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btt_dashboard_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btt_baocao_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sidebar_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
